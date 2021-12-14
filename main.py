6ce5140f06cc602a3d28eccf8cd0214d9ffcf8ae
wongzz <34603964+w0ngzz@users.noreply.github.com>
2021-12-14 21:06:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 import requests,re,json,os,sys
-import win32api
-import win32con
-import win32gui
 
-def setWallpaper(image_path):
-    key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
-    win32api.RegSetValueEx(key, "WallpaperStyle", 0, win32con.REG_SZ, "2")
-    win32api.RegSetValueEx(key, "TileWallpaper", 0, win32con.REG_SZ, "0")
-    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, image_path, 1 + 2)
-'''
-def setWallPaperBMP(imagePath):
-    bmpImage = Image.open(imagePath)
-    newPath = imagePath.replace('.jpg', '.bmp')
-    bmpImage.save(newPath, "BMP")
-    setWallpaper(newPath)
-'''
 req = requests.get("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1").json()
 urlbase = req['images'][0]['urlbase']
 url='http://cn.bing.com/'+urlbase+'_UHD.jpg'
